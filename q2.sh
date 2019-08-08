b0b218a3b4ee88921878211684ffc17ae5abba0e
SARBAJIT-2019-2024 <53304758+SARBAJIT-2019-2024@users.noreply.github.com>
2019-08-08 23:35:12 +0530

    Add files via upload

---
+++ q2.sh
@@ -0,0 +1,10 @@
+echo " Hey! enter your name again"
+read name 
+mkdir $name 
+touch $name/"name$"1.cpp
+touch $name/"name$"2.cpp
+touch $name/"name$"3.cpp
+touch $name/"name$"4.cpp
+touch $name/"name$"5.cpp
+echo "your folder has been created"
+echo  "your file has been created"
